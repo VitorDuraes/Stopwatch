de59a9807d0bf2bb3465bb7ae648f8abbf2d4022
Vitor Durães <111949857+VitorDuraes@users.noreply.github.com>
2024-03-06 09:42:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+
+namespace cronometro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Start(6);
+
+        }
+        static void Menu()
+
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+
+            PreStart(time * multiplier);
+
+
+            Console.WriteLine(data);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            System.Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+
+        }
+    }
+}
